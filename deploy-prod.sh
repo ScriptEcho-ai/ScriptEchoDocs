9b87a22b39d7e57cd14a7a00a23910a983e4b298
胡小根 <hxg@haomo-studio.com>
2024-03-13 18:02:55 +0800

    chore: 增加GTM代码

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 gitbook build
+# 给所有body后面加GTM内容
+sed -i '' 's|<body>|<body><!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8V8L76C" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->|g' _book/*.html
 rsync -avz _book/* haomo@haomo-tech.com:/var/www/html/docs/gen-code-docs/docs/
